be87fffcd21b720b86f9519fce3494dd5831341f
tigelah <lokoportitimao2009@hotmail.com>
2018-05-08 13:44:29 -0300

    Adicionado os tratamentos das classes Forma de Pagamento e Eventos

---
+++ Tcc.DayHappy/Eventos/Evento.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tcc.DayHappy.Dominio.Orcamentos;
+
+namespace Tcc.DayHappy.Dominio.Eventos
+{
+    public class Evento : Entity
+    {
+        public virtual Orcamento CodOrcamento { get; private set; }
+
+        protected Evento() { }
+
+        public Evento(Orcamento codOrcamento)
+        {
+            ValidacaoValoresSetPropriedades(codOrcamento);
+        }
+        private void ValidacaoValoresSetPropriedades(Orcamento codOrcamento)
+        {
+            DomainException.Quando(codOrcamento == null, "Orçamento não foi completado corretamente");
+
+
+            CodOrcamento = codOrcamento;
+        }
+
+        public void Update(Orcamento codOrcamento)
+        {
+            ValidacaoValoresSetPropriedades(codOrcamento);
+        }
+    }
+ 
+}
---
+++ Tcc.DayHappy/StatusEntrega/StatusEntrega.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tcc.DayHappy.Dominio.Eventos;
+
+namespace Tcc.DayHappy.Dominio.StatusEntrega
+{
+    public class StatusEntrega : Entity
+    {
+        public Evento CodEvento { get; private set; }
+        public string DescricaoProblema { get; private set; }
+        public string NivelProblema { get; private set; }
+        public string SituacaoEntrega { get; private set; }
+
+    }
+}
